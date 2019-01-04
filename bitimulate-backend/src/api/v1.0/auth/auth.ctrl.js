2819a9f0f1e55c784607edf8444b93b706b059a2
sen_web <sen_web@xins-iMac.local>
2019-01-04 15:16:36 +0800

    implemented check API

--- bitimulate-backend/src/api/v1.0/auth/auth.ctrl.js
+++ bitimulate-backend/src/api/v1.0/auth/auth.ctrl.js
@@ -1,6 +1,5 @@
 const Joi = require('joi');
 const User = require('db/models/User');
-const token = require('lib/token');
 
 exports.localRegister = async (ctx) => {
   const { body } = ctx.request;
@@ -42,13 +41,13 @@ exports.localRegister = async (ctx) => {
       displayName, email, password
     });
 
-    ctx.body = user;
-    const accessToken = await token.generateToken({
-      user: {
-        _id: user._id,
-        displayName
-      }
-    }, 'user');
+    ctx.body = {
+      displayName,
+      _id: user._id,
+      metaInfo: user.metaInfo
+    };
+    
+    const accessToken = await user.generateToken();
 
     // configure accessToken to httpOnly cookie
     ctx.cookies.set('access_token', accessToken, {
@@ -58,4 +57,70 @@ exports.localRegister = async (ctx) => {
   } catch (e) {
     ctx.throw(500);
   }
+};
+
+exports.localLogin = async (ctx) => {
+  const { body } = ctx.request;
+
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(30)
+  });
+
+  const result = Joi.validate(body, schema);
+
+  if(result.error) {
+    ctx.status = 400;
+    return;
+  }
+
+  const { email, password } = body;
+
+  try {
+    // find user
+    const user = await User.findByEmail(email);
+    
+    if(!user) {
+      // user does not exist
+      ctx.status = 403;
+      return;
+    }
+
+    const validated = user.validatePassword(password);
+    if(!validated) {
+      // wrong password
+      ctx.status = 403;
+      return;
+    }
+
+    const accessToken = await user.generateToken();
+
+    ctx.cookies.set('access_token', accessToken, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+    });
+
+    const { displayName, _id, metaInfo } = user;
+
+    ctx.body = {
+      displayName,
+      _id,
+      metaInfo
+    };
+  } catch (e) {
+    ctx.throw(e);
+  }
+};
+
+exports.check = (ctx) => {
+  const { user } = ctx.request;
+  
+  if(!user) {
+    ctx.status = 403;
+    return;
+  }
+  
+  ctx.body = {
+    user
+  };
 };
